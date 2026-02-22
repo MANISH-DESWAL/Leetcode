29762f8fd30c60deb61cddcbee020cdd08156f79
MANISH-DESWAL <manishdeswal0899@gmail.com>
2026-02-23 04:23:16 +0530

    2sum problrm using dicttionary in py

--- 2sum_Leet.py
+++ 2sum_Leet.py
@@ -1,19 +1,35 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        i=0
-        j=1
-        l1=[]
-        while nums[i]+nums[j]!=target and i!= len(nums)-1:
-            j=j+1
-            if j==len(nums):
-                i+=1
-                j=i+1
-        else :
-            l1.append(i)
-            l1.append(j)
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         i=0
+#         j=1
+#         l1=[]
+#         while nums[i]+nums[j]!=target and i!= len(nums)-1:
+#             j=j+1
+#             if j==len(nums):
+#                 i+=1
+#                 j=i+1
+#         else :
+#             l1.append(i)
+#             l1.append(j)
 
-            return l1    
+#             return l1    
+        
+
+        #2nd way:--
+class Solution:
+            def twoSum(self, nums: List[int], target: int) -> List[int]:
+                dict={}
+                for i in range(0,len(nums)):
+                    # print(f"dict[target-nums[i]]={dict[target-nums[i]]},i={i}")
+                    if target-nums[i]  in dict.keys():
+                        print(f"dict[target-nums[i]]={dict[target-nums[i]]},i={i}")
+                        return [dict[target-nums[i]],i]
+                    
+                    else:
+                        
+                        dict[nums[i]]=i
+                        print(f"dict[target-nums[{i}]]={dict[nums[i]]},target-nums={target-nums[i]}")
             
 
 a=Solution()
-print(a.twoSum([1,2,3,5,6],11))
+print(a.twoSum([3,2,4],6))
